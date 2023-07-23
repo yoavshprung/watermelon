451e7c6649885a03de50c84a392c2af7d75636a7
yoavshprung <132666005+yoavshprung@users.noreply.github.com>
2023-07-23 22:27:38 +0300

    Add files via upload
    Signed-off-by: yoavshprung <132666005+yoavshprung@users.noreply.github.com>

---
+++ app_test.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_index_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, World!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
+
